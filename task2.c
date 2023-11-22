28a525d8abb2e25d5312630efb56f57c2405b287
sOlve13 <Renat597@ukr.net>
2023-11-22 15:20:28 -0800

    task2 changed random

--- task2.c
+++ task2.c
@@ -8,7 +8,7 @@ int list[N];
 void average(){
     double all = 0.0;
     for (int i = 0; i < N; i++) {
-        int a = rand() % 202 - 101;
+        int a = rand() % 201 - 101;
         list[i] = a;
         printf("The list[%d] is %d\n", i, a);
         all += a;
